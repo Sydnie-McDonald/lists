1eaa372a83ff14fa4dfd5ce518f15c33c5e06e08
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-08 16:59:13 -0600

    adding to app.js importing dog js data. creating folders and files

---
+++ dog-data.js
@@ -0,0 +1,22 @@
+let dogs = [
+    {
+        breed: 'American-terrier',
+        weight: '60lbs',
+        color: 'black',
+    },
+    {
+        breed: 'Lab',
+        weight: '40lbs',
+        color: 'brown',
+    },
+    {
+        breed: 'Pitbull',
+        weight: '72lbs',
+        color: 'grey',
+    },
+    {
+        breed: 'Great Dane',
+        weight: '80lbs',
+        color: 'brown',
+    }
+];
